52a17f53b9205cc765d023e2bfd5db42e0323488
warisamir <warisamir1918@gmail.com>
2023-12-17 02:24:35 +0530

    using class selector for n^2 approach

--- Questions/app.js
+++ Questions/app.js
@@ -1,9 +1,23 @@
-const btns=document.querySelectorAll('.question-btn');
-const currentquestion=0;
-btns.forEach(function(btn){
-    btn.addEventListener('click',function(e){
-       const par=e.currentTarget.parentElement.parentElement;
-       console.log(par);
-       par.classList.toggle('show-text')
+// const btns=document.querySelectorAll('.question-btn');
+// const currentquestion=0;
+// btns.forEach(function(btn){
+//     btn.addEventListener('click',function(e){
+//        const par=e.currentTarget.parentElement.parentElement;
+//        console.log(par);
+//        par.classList.toggle('show-text')
+//     })
+// })
+
+//using selectors inside the element
+
+const ques=document.querySelectorAll(".question");
+ques.forEach(function(que){
+    const btn=que.querySelector(".question-btn");
+    btn.addEventListener("click",function(){
+    ques.forEach(function(i){
+      if(i!==que){
+      i.classList.remove('show-text');}
     })
+     que.classList.toggle('show-text');
+   })
 })
